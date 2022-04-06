2582e3c4045a02ae99973a67ec78f861500a1c4a
Thanh Hoang <thanhhoang4869@gmail.com>
2022-04-06 23:27:19 +0700

    view prd detail by id

--- middlewares/route.mdw.js
+++ middlewares/route.mdw.js
@@ -1,17 +1,15 @@
-import express from "express";
 import indexRoute from '../routes/index.route.js'
 import aboutRoute from '../routes/about.route.js'
-
-// import productRoute from '../routes/product.route.js'
-// import adminRoute from '../routes/admin.route.js'
 import accountRoute from '../routes/account.route.js'
+import productRoute from '../routes/product.route.js'
+
 // import sellerRoute from '../routes/seller.route.js'
 // import auth from '../middlewares/auth.mdw.js';
 
 export default function (app) {
     app.use('/', indexRoute);
     app.use('/about', aboutRoute);
-    // app.use('/admin', auth, adminRoute);
+    app.use('/product', productRoute);
     app.use('/account', accountRoute);
     // app.use('/profile', auth, accountRoute);
     // app.use('/seller', auth,sellerRoute);
