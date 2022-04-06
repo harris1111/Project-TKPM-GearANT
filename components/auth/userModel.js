4826d4992645d41b26833f3fbf654bc800795a1e
harris1111 <minhan112001@gmail.com>
2022-04-07 02:05:54 +0700

    js code for signin, signup, logout

--- components/auth/userModel.js
+++ components/auth/userModel.js
@@ -1,13 +1,39 @@
-const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  email_address: String,
-  phone: String,
-  address: String,
-  isBan: Boolean,
-  activationString: String,
-});
-module.exports = mongoose.model("User", userSchema);
+import db from '../utils/db.js';
 
+export default {
 
+    async findById(id) {
+        const list = await db('users').where('id', id);
+        if (list.length === 0)
+            return null;
+
+        return list[0];
+    },
+
+    async findByUsername(username) {
+        const list = await db('users').where('username', username);
+        if (list.length === 0)
+            return null;
+
+        return list[0];
+    },
+
+    add(entity) {
+        return db('users').insert(entity);
+    },
+
+    del(id) {
+        return db('users')
+            .where('id', id)
+            .del();
+    },
+
+    patch(entity) {
+        const id = entity.id;
+        delete entity.id;
+
+        return db('users')
+            .where('id', id)
+            .update(entity);
+    },
+}
--- routes/account.route.js
+++ routes/account.route.js
@@ -1,10 +1,39 @@
 import express from 'express';
-
+import bcrypt from 'bcrypt';
+import moment from 'moment';
+import userModel from '../model/user.model.js';
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('product/product');
+router.get('/register', async function(req, res) {
+
+    res.render('vwAccount/register.hbs', {
+        layout: 'auth.hbs'
+    });
 });
+
+router.post('/register', async function(req, res) {
+    const rawPassword = req.body.password;
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(rawPassword, salt);
+    const user = {
+        username: req.body.username,
+        password: hash,
+        dob: dob,
+        address: req.body.address,
+        number: req.body.number,
+        permission: 1
+    }
+    await userModel.add(user);
+});
+
+router.get('/is-available', async function(req, res) {
+    const username = req.query.user;
+    const user = await userModel.findByUsername(username);
+    if (user === null) {
+        return res.json(true);
+    }
+    res.json(false);
+});
+
 
 export default router;
