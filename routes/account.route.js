078964aca92f22a2e023208752dd685f9b074006
19127049 <huongngan2001@gmail.com>
2022-04-07 22:32:28 +0700

    add badge

--- routes/account.route.js
+++ routes/account.route.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('about');
+    res.render('account');
 });
 
 router.get('/cart', function(req, res, next) {
