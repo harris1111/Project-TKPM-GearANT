5a3a5dec021c0262b8a5d9e10ae3468842ea3670
harris1111 <minhan112001@gmail.com>
2022-04-07 21:29:03 +0700

    clean code

--- routes/account.route.js
+++ routes/account.route.js
@@ -4,36 +4,6 @@ import moment from 'moment';
 import userModel from '../components/auth/userModel.js';
 const router = express.Router();
 
-router.get('/register', async function(req, res) {
-
-    res.render('vwAccount/register.hbs', {
-        layout: 'auth.hbs'
-    });
-});
-
-router.post('/register', async function(req, res) {
-    const rawPassword = req.body.password;
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(rawPassword, salt);
-    const user = {
-        username: req.body.username,
-        password: hash,
-        dob: dob,
-        address: req.body.address,
-        number: req.body.number,
-        permission: 1
-    }
-    await userModel.add(user);
-});
-
-router.get('/is-available', async function(req, res) {
-    const username = req.query.user;
-    const user = await userModel.findByUsername(username);
-    if (user === null) {
-        return res.json(true);
-    }
-    res.json(false);
-});
 
 
 export default router;
