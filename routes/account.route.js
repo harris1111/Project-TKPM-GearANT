5a91a3dc44edeac03c8598dae4908c19a95e7a9b
19127049 <huongngan2001@gmail.com>
2022-04-07 23:39:50 +0700

    upload account + checkout

--- routes/account.route.js
+++ routes/account.route.js
@@ -4,11 +4,15 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('account');
+    res.render('account/account');
 });
 
 router.get('/cart', function(req, res, next) {
     res.render('account/cart');
+});
+
+router.get('/checkout', function(req, res, next) {
+    res.render('account/checkout');
 });
 
 
