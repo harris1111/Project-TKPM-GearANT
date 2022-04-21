fcf995ec4a7452fdc7ae88d33ed36ca55d5d6754
Nguyễn Minh An <minhan112001@gmail.com>
2022-04-21 15:57:36 +0700

    add p/c Ative for layout

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -3,54 +3,67 @@ import adminModel from "../models/admin.model.js";
 
 const router = express.Router();
 
-router.get("/user", function(req, res, next) {
-    let uActive = true;
-    res.render("admin/user", {
-        uActive,
-        layout: "admin.hbs",
-    });
+router.get("/user", function (req, res, next) {
+  let uActive = true;
+  res.render("admin/user", {
+    uActive,
+    layout: "admin.hbs",
+  });
 });
 
-router.get("/product", function(req, res, next) {
-    let pActive = true;
-    res.render("admin/product", {
-        pActive,
-        layout: "admin.hbs",
-    });
+router.get("/product", function (req, res, next) {
+  let pActive = true;
+  res.render("admin/product", {
+    pActive,
+    layout: "admin.hbs",
+  });
 });
 
-router.get("/order", function(req, res, next) {
-    let oActive = true;
-    res.render("admin/order", {
-        oActive,
-        layout: "admin.hbs",
-    });
+router.get("/order", function (req, res, next) {
+  let oActive = true;
+  res.render("admin/order", {
+    oActive,
+    layout: "admin.hbs",
+  });
 });
-router.post("/product/edit-product", async function(req, res) {
-    const name = res.body.newProductName;
-    const price = res.body.newPrice;
-    const count = res.body.newCount;
-    const user = {
-        Username: req.session.authUser.username,
-    };
-    if (name != "") user = user + { ProName: name };
-    if (price != "") user = user + { Price: price };
-    if (count != "") user = user + { Stock: count };
-    await adminModel.updateAdmin(user);
-    return res.render('/');
+router.post("/product/add-product", async function(req,res) { 
+
 });
-router.post("/product/delete-product", async function(req,res) {
-    const id = req.body.delete;
-    await adminModel.delProduct(id);
-    return res.redirect("/admin/product");
+router.post("/product/edit-product", async function (req, res) {
+  const name = res.body.newProductName;
+  const price = res.body.newPrice;
+  const count = res.body.newCount;
+  const user = {
+    Username: req.session.authUser.username,
+  };
+  if (name != "") user = user + { ProName: name };
+  if (price != "") user = user + { Price: price };
+  if (count != "") user = user + { Stock: count };
+  await adminModel.updateAdmin(user);
+  return res.render("/admin/product", {
+    pAtive,
+    layout: "admin.hbs",
+  });
 });
-router.post("/product/set-state-order", async function(req,res) { 
-     const state = req.body.state;
-     const OrderID = req.body.orderid;
-     const user = {
-        orderID: OrderID,
-        State: state 
-     }
-     await adminModel.updateStateOrder(user);
+router.post("/product/delete-product", async function (req, res) {
+  const id = req.body.delete;
+  await adminModel.delProduct(id);
+  return res.render("/admin/product", {
+    pAtive,
+    layout: "admin.hbs",
+  });
+});
+router.post("/order/set-state-order", async function (req, res) {
+  const state = req.body.state;
+  const OrderID = req.body.orderid;
+  const user = {
+    orderID: OrderID,
+    State: state,
+  };
+  await adminModel.updateStateOrder(user);
+  return res.render("/admin/order", {
+    oAtive,
+    layout: "admin.hbs",
+  });
 });
 export default router;
