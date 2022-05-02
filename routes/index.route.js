0c5923775564293a90af286645e11f87e4a10847
Nguyễn Minh An <minhan112001@gmail.com>
2022-05-02 23:57:34 +0700

    Update https and recaptcha v2

--- routes/index.route.js
+++ routes/index.route.js
@@ -5,62 +5,84 @@ import constant from "../utils/config.js";
 import userModel from "../models/user.model.js";
 import emailModel from "../models/email.model.js";
 import bcrypt from "bcrypt";
+import reCapt from "express-recaptcha";
+
+const RC = reCapt.RecaptchaV2;
+const recaptcha = new RC(
+  "6Lf3qxgfAAAAABMD65zmxa6hBnk9dtzUduPW92Fr",
+  "6Lf3qxgfAAAAAK-UWhfC28ZcAwZBOKLjlhjI1-rl",
+  { callback: "cb" }
+);
 const router = express.Router();
 
 router.use(bodyParser.urlencoded({ extended: false }));
 
 /* GET home page. */
-router.get("/", async function(req, res, next) {
-    const best_ram = await productHome.findBestSeller(constant.bigCatID.RAM);
-    const best_ssd = await productHome.findBestSeller(constant.bigCatID.SSD);
-    const best_cpu = await productHome.findBestSeller(constant.bigCatID.CPU);
+router.get("/", async function (req, res, next) {
+  const best_ram = await productHome.findBestSeller(constant.bigCatID.RAM);
+  const best_ssd = await productHome.findBestSeller(constant.bigCatID.SSD);
+  const best_cpu = await productHome.findBestSeller(constant.bigCatID.CPU);
 
-    for(let i in best_ram){
-        const sold = await productHome.findSold(best_ram[i].ProID)
-        best_ram[i].sold = sold.Sold || 0
-    }
+  for (let i in best_ram) {
+    const sold = await productHome.findSold(best_ram[i].ProID);
+    best_ram[i].sold = sold.Sold || 0;
+  }
 
-    for(let i in best_ssd){
-        const sold = await productHome.findSold(best_ssd[i].ProID)
-        best_ssd[i].sold = sold.Sold || 0
-    }
+  for (let i in best_ssd) {
+    const sold = await productHome.findSold(best_ssd[i].ProID);
+    best_ssd[i].sold = sold.Sold || 0;
+  }
 
-    for(let i in best_cpu){
-        const sold = await productHome.findSold(best_cpu[i].ProID)
-        best_cpu[i].sold = sold.Sold || 0
-    }
+  for (let i in best_cpu) {
+    const sold = await productHome.findSold(best_cpu[i].ProID);
+    best_cpu[i].sold = sold.Sold || 0;
+  }
 
-    res.render("index", {
-        layout:'homecat.hbs',
-        best_ram,
-        best_ssd,
-        best_cpu,
-    });
+  res.render("index", {
+    layout: "homecat.hbs",
+    best_ram,
+    best_ssd,
+    best_cpu,
+  });
 });
 
 // login started
-router.get("/login", async function(req, res, next) {
+router.get(
+  "/login",
+  recaptcha.middleware.render,
+  async function (req, res, next) {
     if (req.session.auth === true) {
-        return res.redirect("/");
+      return res.redirect("/");
     } else return res.render("login");
-});
-
-router.post("/login", async function(req, res) {
+  }
+);
+router.get(
+  "/register",
+  recaptcha.middleware.render,
+  async function (req, res, next) {
+    if (req.session.auth === true) {
+      return res.redirect("/");
+    } else return res.render("login");
+  }
+);
+router.post("/login", recaptcha.middleware.verify, async function (req, res) {
+  console.log(req.recaptcha.error);
+  if (!req.recaptcha.error) {
     const username = req.body.username;
     const user = await userModel.findByUsername(username);
 
     if (user === null) {
-        return res.render("login", {
-            error_login: "Invalid username or password!",
-        });
+      return res.render("login", {
+        error_login: "Invalid username or password!",
+      });
     }
 
     const ret = bcrypt.compareSync(req.body.password, user.Password);
 
     if (ret === false) {
-        return res.render("login", {
-            error_login: "Invalid username or password!",
-        });
+      return res.render("login", {
+        error_login: "Invalid username or password!",
+      });
     }
 
     delete user.Password;
@@ -68,23 +90,32 @@ router.post("/login", async function(req, res) {
     req.session.auth = true;
     req.session.authUser = user;
 
-    if(user.Type==='2'){
-        req.session.isAdmin = true;
-        res.locals.isAdmin=req.session.isAdmin;
+    if (user.Type === "2") {
+      req.session.isAdmin = true;
+      res.locals.isAdmin = req.session.isAdmin;
     }
 
-    const url = req.session.retUrl || '/';
+    const url = req.session.retUrl || "/";
     res.redirect(url);
+  } else {
+    return res.render("login", {
+      error_login: "Please verify the captcha!",
+    });
+  }
 });
 //login ended
 
 //register started
-router.post("/register", async function(req, res) {
-    const rawPassword = req.body.password;
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(rawPassword, salt);
-    const otp = emailModel.sendOTPRegister(req.body.email);
-    const user = {
+router.post(
+  "/register",
+  recaptcha.middleware.verify,
+  async function (req, res) {
+    if (!req.recaptcha.error) {
+      const rawPassword = req.body.password;
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(rawPassword, salt);
+      const otp = emailModel.sendOTPRegister(req.body.email);
+      const user = {
         Email: req.body.email,
         Username: req.body.username,
         Name: req.body.name,
@@ -94,114 +125,121 @@ router.post("/register", async function(req, res) {
         Type: 1,
         Valid: false,
         OTP: otp,
-    };
+      };
 
-    await userModel.add(user);
-    req.session.registerUser = req.body.email;
-    res.redirect("/confirm-register");
-});
-
-router.post('/confirm-register', async function(req, res) {
-    const email = req.session.registerUser;
-    const ret = await userModel.findOTP(email);
-    const otpInput = req.body.OTP;
-
-    if(otpInput !== ret.OTP){
-        return res.render('otp/confirm-otp-register',{
-            error: 'OTP is incorrect!'
-        });
-    }
-    req.body.Email = email;
-    req.body.OTP = 'NULL';
-    req.body.Valid = true;
-    await userModel.updateUser(req.body);
-
-    req.session.registerUser = null;
-    return res.redirect('/register-success');
-});
-router.get('/username-available', async function(req, res) {
-    const username = req.query.username;
-    const user = await userModel.findByUsername(username);
-    if (user === null) {
-        return res.json(true);
+      await userModel.add(user);
+      req.session.registerUser = req.body.email;
+      res.redirect("/confirm-register");
     } else {
-        return res.json(false);
+      const rActive = true;
+      return res.render("login", {
+        error_register: "Please check the captcha!",
+        rActive
+      });
     }
+  }
+);
+
+router.post("/confirm-register", async function (req, res) {
+  const email = req.session.registerUser;
+  const ret = await userModel.findOTP(email);
+  const otpInput = req.body.OTP;
+  if (otpInput !== ret.OTP) {
+    return res.render("otp/confirm-otp-register", {
+      error: "OTP is incorrect!",
+    });
+  }
+  req.body.Email = email;
+  req.body.OTP = "NULL";
+  req.body.Valid = true;
+  await userModel.updateUser(req.body);
+
+  req.session.registerUser = null;
+  return res.redirect("/register-success");
+});
+router.get("/username-available", async function (req, res) {
+  const username = req.query.username;
+  const user = await userModel.findByUsername(username);
+  if (user === null) {
+    return res.json(true);
+  } else {
+    return res.json(false);
+  }
 });
 //register ended
 
 //otp
-router.get('/forgot-password', async function(req, res) {
-    res.render('otp/forgot-password');
+router.get("/forgot-password", async function (req, res) {
+  res.render("otp/forgot-password");
 });
-router.get('/confirm-register', async function(req, res) {
-    res.render('otp/confirm-otp-register');
-});
-
-router.get('/reset-success', async function(req, res) {
-    res.render('otp/reset-success');
+router.get("/confirm-register", async function (req, res) {
+  res.render("otp/confirm-otp-register");
 });
 
-router.get('/register-success', async function(req, res) {
-    res.render('otp/register-success');
+router.get("/reset-success", async function (req, res) {
+  res.render("otp/reset-success");
 });
 
-router.get('/confirm-otp', async function(req, res) {
-    res.render('otp/confirm-otp');
+router.get("/register-success", async function (req, res) {
+  res.render("otp/register-success");
 });
-router.post('/forget-password', async function(req, res) {
-    const email = req.body.Email;
-    const user = await userModel.findByEmail(email);
-    if(user === null){
-        return res.render('otp/forgot-password',{
-            error: 'Email not found. Please try again!'
-        });
-    }
 
-    req.session.forgetUser = email;
-    const otp = emailModel.sendOTP(email);
-    req.body.OTP = otp.toString();
-    await userModel.updateUser(req.body);
-    return res.redirect('/confirm-otp');
+router.get("/confirm-otp", async function (req, res) {
+  res.render("otp/confirm-otp");
+});
+router.post("/forget-password", async function (req, res) {
+  const email = req.body.Email;
+  const user = await userModel.findByEmail(email);
+  if (user === null) {
+    return res.render("otp/forgot-password", {
+      error: "Email not found. Please try again!",
+    });
+  }
+
+  req.session.forgetUser = email;
+  const otp = emailModel.sendOTP(email);
+  req.body.OTP = otp.toString();
+  await userModel.updateUser(req.body);
+  return res.redirect("/confirm-otp");
 });
 // otp ended
 
-router.post('/confirm-otp', async function(req, res) {
-    const email = req.session.forgetUser;
-    //console.log(email);
-    const ret = await userModel.findOTP(email);
-    const otpInput = req.body.OTP;
+router.post("/confirm-otp", async function (req, res) {
+  const email = req.session.forgetUser;
+  //console.log(email);
+  const ret = await userModel.findOTP(email);
+  const otpInput = req.body.OTP;
 
-    if(otpInput !== ret.OTP){
-        return res.render('otp/confirm-otp',{
-            error: 'OTP is incorrect!'
-        });
-    }
-    const password = emailModel.sendNewPassword(email);
-    req.body.Email = email;
-    req.body.OTP = 'NULL';
-    req.body.Password = password;
-    await userModel.updateUser(req.body);
+  if (otpInput !== ret.OTP) {
+    return res.render("otp/confirm-otp", {
+      error: "OTP is incorrect!",
+    });
+  }
+  const password = emailModel.sendNewPassword(email);
+  req.body.Email = email;
+  req.body.OTP = "NULL";
+  req.body.Password = password;
+  await userModel.updateUser(req.body);
 
-    req.session.forgetUser = null;
-    return res.redirect('/reset-success');
+  req.session.forgetUser = null;
+  return res.redirect("/reset-success");
 });
 
 //log out
-router.post("/logout", async function(req, res) {
-    req.session.auth = false;
-    req.session.authUser = null;
-    req.session.retUrl = null;
+router.post("/logout", async function (req, res) {
+  req.session.auth = false;
+  req.session.authUser = null;
+  req.session.retUrl = null;
 
-    //req.session.isSeller = null;
-    req.session.isAdmin = null;
+  //req.session.isSeller = null;
+  req.session.isAdmin = null;
 
-    res.locals.auth = req.session.auth;
-    res.locals.authUser = req.session.authUser;
-    //res.locals.isSeller = req.session.isSeller;
-    res.locals.isAdmin = req.session.isAdmin;
+  res.locals.auth = req.session.auth;
+  res.locals.authUser = req.session.authUser;
+  //res.locals.isSeller = req.session.isSeller;
+  res.locals.isAdmin = req.session.isAdmin;
 
-    res.redirect("/");
+  res.redirect("/");
 });
 //log out
 export default router;
